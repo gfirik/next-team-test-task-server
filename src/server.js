e649e472bdb400a24eff4dba12bfc035e4b45dde
gfirik <firdavs.gafurjonov@gmail.com>
2023-01-30 14:38:41 +0900

    fix: bug

--- src/server.js
+++ src/server.js
@@ -11,27 +11,17 @@ const { numberRouter, numbers } = require("./routes/number");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Serve static files from the 'static' directory
-app.use(express.static("static"));
 
 // Use the message, number and upload routes
 app.use("/message", messageRouter);
 app.use("/number", numberRouter);
 
 app.get("/", function (req, res) {
-  // Get files from the 'static' folder
-  fs.readdir("static", (err, files) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Error reading files");
-    } else {
-      // Get the array from routes/message
-      const data = {};
-      if (numbers.length) data.numbers = numbers;
-      if (messages.length) data.messages = messages;
-      res.json({ data });
-    }
-  });
+  // Get the array from routes/message
+  const data = {};
+  if (numbers.length) data.numbers = numbers;
+  if (messages.length) data.messages = messages;
+  res.json({ data });
 });
 
 app.listen(port, () => {
